d516fd374e95a0467de8cadfa80172e9817201f5
TobiasMaugus <tobiasmaugus705@gmail.com>
2025-10-13 13:54:30 -0300

    speed + params via txt

--- src/Principal.java
+++ src/Principal.java
@@ -1,8 +1,6 @@
 public class Principal{
   public static void main(String[] args){
     Simulator simulator = new Simulator();
-    simulator.addLake(20, 20, 10, 12);
-    simulator.addLake(30, 35, 8, 10);
     simulator.runLongSimulation();
   }
 }
